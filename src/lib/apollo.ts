cdb327a99c662cbfdabe76be7b060ccc3d7291f5
juniorvilas <log_junior@hotmail.com>
2022-07-14 13:23:19 -0300

    add authorization no headers para criação do subscribe

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ofo59p00ty01z4czeggc9l/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}` 
+    },
     cache: new InMemoryCache()
 });
